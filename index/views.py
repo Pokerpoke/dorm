369400fe7f557c753d0a1043119909daf8086b4e
pokerpoke <j824544269@gmail.com>
2017-04-01 11:38:13 +0800

    add card template

--- index/views.py
+++ index/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
+from query.forms import DevRoomForm
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    form = DevRoomForm()
+    card1 = {'title': '开灯',
+             'color': 'blue',
+             'content': 'Turn on your light.',
+             'action1': {'url': '/queryall/', 'name': '查询'},
+             'action2': {'url': '/queryall/', 'name': '查询'}}
+    card2 = {'title': '开灯',
+             'color': 'green',
+             'content': 'Turn on your light.',
+             'action1': {'url': '/queryall/', 'name': '查询'},
+             'action2': {'url': '/queryall/', 'name': ''}}
+    cards = []
+    cards.append(card1)
+    cards.append(card2)
+    return render(request, 'index.html', locals())
---
+++ query/forms.py
@@ -0,0 +1,11 @@
+"""
+Forms that get data from database
+"""
+from django import forms
+
+
+class DevRoomForm(forms.Form):
+    """
+    Forms of dev---roomname query
+    """
+    dev = forms.CharField(label='设备ID', max_length=100)
