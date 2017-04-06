c58bf399070be1f39235807d6ec9fce68caba7e8
pokerpoke <j824544269@gmail.com>
2017-04-06 20:34:49 +0800

    添加supervisor配置文件，更新TODO

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ dorm
 - [x] 更新后的数据格式
 - [x] Django+nginx+uwsgi部署
 - [x] 数据库删除指令
-- [ ] supervisor配置文件
+- [x] supervisor配置文件(uwsgi、nginx、serialrecv)
+- [ ] 打印日志
+- [ ] 网页监视程序运行状态
 - [ ] 网页发送下行指令
 - [ ] 一键部署
--- serialrecv.py
+++ serialrecv.py
@@ -1,12 +1,13 @@
+#!/usr/bin/python3
 #-*- coding:utf-8 -*-
 """
 Raspberry Pi serial receive and write to mysql
 """
 
 import time
-import serial
 import sqlite3
 import binascii
+import serial
 
 SER = serial.Serial('/dev/ttyUSB0', 115200, timeout=0, interCharTimeout=0.001)
 
@@ -27,7 +28,7 @@ class RecevData(object):
         self.relay = 0
         self.stop = 0
 
-    def infoMatch(self, data):
+    def info_match(self, data):
         """
         Match infomation from receive message.
 
@@ -53,14 +54,14 @@ class RecevData(object):
         elif ((cmp(data[0], "\xf3") == 0) and
               (cmp(data[12], "\xfc") == 0)):
             # Ask for network
+            # Send devID + 'FF' to device to comform received
+            back = data[1:3] + "FF"
+            SER.write(back)
             ptr = 0
             # Get the number of relays
             for i in data[7:12]:
                 if i != '\xf6':
                     ptr = ptr + 1
-            # Send devID + 'FF' to device to comform received
-            back = data[1:3] + "FF"
-            SER.write(back)
             # Get infomation matched
             self.messageType = binascii.hexlify(data[0])
             self.devID = binascii.hexlify(data[1:3])
@@ -71,9 +72,9 @@ class RecevData(object):
             self.router = binascii.hexlify(
                 data[7:7 + ptr] + data[4] + data[8 + ptr:12])
             if data[3] == '0':
-                ansF4Relay(data,ptr)
+                ans_f4_relay(data, ptr)
             else:
-                ansF4(data)
+                ans_f4_dev(data)
             return True
         elif ((cmp(data[0], "\xf5") == 0) and
               (cmp(data[12], "\xfa") == 0)):
@@ -83,61 +84,67 @@ class RecevData(object):
             return False
 
 
-def ansF4Relay(data, ptr):
+def ans_f4_relay(data, ptr):
     """
     Answer \xf4 ... \xfb data to relay
     replace data[7:12] with router
     """
     back = "\xf4" + "FF" + data[3:6] + "\x07" + \
         data[7:7 + ptr] + data[4] + data[8 + ptr:12] + "\xfb"
-    print('back:   ',binascii.hexlify(back))
+    print('back:   ', binascii.hexlify(back))
     SER.write(back)
     return True
 
 
-def ansF4(data):
+def ans_f4_dev(data):
+    """
+    Answer common f4
+    """
     back = "\xf4" + "FF" + data[3:6] + "\x07" + data[7:12] + "\xfb"
+    print('back:   ', binascii.hexlify(back))
     SER.write(back)
     return True
 
 
-def readSerial(serial):
+def read_serial(ser):
     """
-        Read serial and write into mysql
+    Read serial and write into db.sqlite3.
     """
-    if SER.inWaiting() < 13:
+    if ser.inWaiting() < 13:
         return False
     else:
-        data = SER.read(13)
-        print('receive:',binascii.hexlify(data))
-        SER.flushInput()
-        rec = RecevData()
-        if rec.infoMatch(data) == True:
-            conn = sqlite3.connect('db.sqlite3')
-            cursor = conn.cursor()
-            # cursor.execute("insert into dormdb_dorm (devID,\
-            #         devName,devStatus,nRelays,relay1,relay2,\
-            #         relay3,relay4,relay5,time)\
-            #         values (", rec.devID, ",", rec.devName, ",", rec.devStatus,
-            #                ",", rec.distance, ",", rec.relay1, ",", rec.relay2,
-            #                ",", rec.relay3, ",", rec.relay4, ",", rec.relay5,
-            #                ", datetime('now','localtime'))")
-            cursor.execute("insert into dormdb_dorm (devID,\
-                    devStatus,nRelays,relay,time)\
-                    values ('" + str(rec.devID) +
-                           "','" + str(rec.devStatus) +
-                           "','" + str(rec.nRelays) +
-                           "','" + str(rec.relay) +
-                           "',datetime('now','localtime'))")
-            cursor.close()
-            conn.commit()
-            conn.close()
-            return rec
-        else:
-            return False
+        r_data = SER.readline()
+        for i, temp_data in enumerate(r_data):
+            if (temp_data == '\xf1') or (temp_data == '\xf3'):
+                data = r_data[i:i + 13]
+                print('receive:', binascii.hexlify(data))
+                ser.flushInput()
+                rec = RecevData()
+                if rec.info_match(data) is True:
+                    write_router_to_database(rec)
+                    return True
+            else:
+                return False
 
+
+def write_router_to_database(data):
+    """
+    Write data into database.
+    """
+    conn = sqlite3.connect('db.sqlite3')
+    cursor = conn.cursor()
+    cursor.execute("insert into dormdb_dorm (devID,\
+            devStatus,nRelays,relay,time)\
+            values ('" + str(data.devID) +
+                   "','" + str(data.devStatus) +
+                   "','" + str(data.nRelays) +
+                   "','" + str(data.relay) +
+                   "',datetime('now','localtime'))")
+    cursor.close()
+    conn.commit()
+    conn.close()
 
 if __name__ == '__main__':
     while True:
-        readSerial(SER)
+        read_serial(SER)
         time.sleep(0.0001)
